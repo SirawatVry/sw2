c825f8aa2e64dd0c19c0c2036d8ed9d55760f4bb
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-07-17 09:57:19 +0700

    Add files via upload

---
+++ sw3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+		string Name;
+		int Birth;
+		cout << "Enter Name :";
+		cin >> Name;
+		cout <<"Enter Year of birth :";
+		cin >> Birth;
+		cout << "Your Age =" <<2566 - Birth<<endl;
+		return(0);
+}
